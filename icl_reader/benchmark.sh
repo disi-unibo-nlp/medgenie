10ba0d397e34ccdffcc1fbe1eae1404e37745ca6
pr3ss <alex.presepi01@gmail.com>
2024-02-22 11:09:51 +0100

    Fix dataset split

--- icl_reader/benchmark.sh
+++ icl_reader/benchmark.sh
@@ -1,6 +1,7 @@
 python3 benchmark.py \
     --model_name HuggingFaceH4/zephyr-7b-beta \
     --dataset_name medqa \
+    --test_set \
     --n_options 4 \
     --batch_size 8 \
     --max_context_window 4000 
